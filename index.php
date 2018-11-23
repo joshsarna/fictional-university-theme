9983958fc243057bfbafed74fa50c471fcb1038e
joshsarna <joshsarna@gmail.com>
2018-11-23 14:07:46 -0800

    create an array

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-  function greet($name, $color) {
-    echo "<p>Hi, my name is $name, and my favorite color is $color.</p>";
-  }
-
-  greet('John', 'blue');
-  greet('Jane', 'green');
+  $names = array('Josh', 'Maria', 'Frank', 'Davis');
 ?>
 
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
+<!-- <p>Hi, my name is <?php echo $names[0]; ?></p> -->
+
