0169026b3362a540d93a4fbfeb6304f21fc70f5c
joshsarna <joshsarna@gmail.com>
2018-11-23 14:09:37 -0800

    create a while loop

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
   $names = array('Josh', 'Maria', 'Frank', 'Davis');
+
+  $count = 1;
+
+  while($count <= 100) {
+    echo "<li>$count</li>";
+    $count++;
+  }
 ?>
 
 <!-- <p>Hi, my name is <?php echo $names[0]; ?></p> -->
