54ffe42aaa1d07d82923c915677919d934ab632a
joshsarna <joshsarna@gmail.com>
2018-12-06 15:00:23 -0800

    make api key functionality in functions.php

--- functions.php
+++ functions.php
@@ -70,4 +70,11 @@
   }
 
   add_action('pre_get_posts', 'university_adjust_queries');
+
+  function universityMapKey ($api) {
+    $api['key'] = '<API KEY>';
+    return $api;
+  }
+
+  add_filter('acf/fields/google_map/api', 'universityMapKey');
 ?>
